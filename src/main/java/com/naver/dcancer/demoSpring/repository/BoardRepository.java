74c3aa035ddf8cb011d1690d0f9d70d8dbfb1c18
dewey <dewey@deweyui-Macmini.local>
2024-10-30 16:33:49 +0900

    restart

--- src/main/java/com/naver/dcancer/demoSpring/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.naver.dcancer.demoSpring.repository;
-
-import com.naver.dcancer.demoSpring.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
